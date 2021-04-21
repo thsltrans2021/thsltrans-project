76cde58cf2f5fa1408bd484112a3354fd7109c8e
Nutthanich Narphromar <monaprom134@gmail.com>
2021-04-21 15:26:57 +0700

    Update a navbar to version 3 and add a script for changing scene

---
+++ ThSLtransProject/Assets/Scripts/UINavbar.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UINavbar : MonoBehaviour
+{
+    public void ChangeScene(string sceenName)
+    {
+        SceneManager.LoadScene(sceenName);
+    }
+
+    public void Signout()
+    {
+        // TODO
+        print("Please implement this method");
+    }
+}
