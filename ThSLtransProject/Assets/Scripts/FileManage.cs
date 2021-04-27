f7539aa7673111795d1feab59841e7ca2bcac08b
“Wijantra” <wijantra.c@ku.th>
2021-04-27 19:53:16 +0700

    create main before and main after scene

---
+++ ThSLtransProject/Assets/Scripts/FileManage.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class FileManage : MonoBehaviour
+{
+    string path;
+    string namePDF = "test";
+
+    public void BrowseFile()
+    {
+        path = EditorUtility.OpenFilePanel("Show","", "pdf");
+        // StartCoroutine(openPDF());
+    }
+
+    void openPDF()
+    {
+        TextAsset pdfTem = Resources.Load("PDFs/"+namePDF, typeof(TextAsset)) as TextAsset;
+        System.IO.File.WriteAllBytes(Application.persistentDataPath + "/"+namePDF+".pdf", pdfTem.bytes);
+        Application.OpenURL(Application.persistentDataPath+"/"+namePDF+".pdf");
+    }
+}
