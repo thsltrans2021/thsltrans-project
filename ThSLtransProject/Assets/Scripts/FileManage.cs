22363c3aace74bebce8f38672ca73ed35498912b
Wijantra <ding533@hotmail.com>
2021-12-28 10:25:43 +0700

    Add signup, login scene and read txt

--- ThSLtransProject/Assets/Scripts/FileManage.cs
+++ ThSLtransProject/Assets/Scripts/FileManage.cs
@@ -4,17 +4,47 @@ using UnityEngine;
 using UnityEditor;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using System;
+using System.IO;
+using System.Linq;
 
 public class FileManage : MonoBehaviour
 {
     string path;
     string namePDF = "test";
+    public Transform contentWindow;
+    public GameObject recallTextObject;
+
+    public void stratText() {
+        Console.WriteLine(" Enter Your Name:"); 
+
+        // string readFromFilePath = Application.streamingAssetsPath + "test" + ".txt";
+        string readFromFilePath = "Assets/Resources/test.txt";
+
+        // string[] readText = File.ReadAllLines(path);
+
+        List<string> fineLines = File.ReadAllLines(readFromFilePath).ToList();
+        // foreach (string s in readText)
+        // {
+        //     Console.WriteLine(s);
+        // }
+
+        foreach (string line in fineLines){
+            Instantiate(recallTextObject, contentWindow);
+            recallTextObject.GetComponent<Text>().text += (line + "\n" );
+
+            Debug.Log(line);
+        }
+
+    }
 
     public void BrowseFile()
     {
         path = EditorUtility.OpenFilePanel("Show","", "pdf");
+    
         // StartCoroutine(openPDF());
     }
+
 
     void openPDF()
     {
@@ -23,3 +53,4 @@ public class FileManage : MonoBehaviour
         Application.OpenURL(Application.persistentDataPath+"/"+namePDF+".pdf");
     }
 }
+
