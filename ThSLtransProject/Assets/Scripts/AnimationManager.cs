1136dee661f412e3efceec932ae274347bee4b50
Nutthanich Narphromar <nutthanich.na@gmail.com>
2022-01-21 12:44:30 +0700

    Add Rain model and animation manager script

---
+++ ThSLtransProject/Assets/Scripts/AnimationManager.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class AnimationManager
+{
+
+    private Animator avatarAnimator;
+    private string animCtrlParamName = "playSentence";
+
+    private const string ANIMATION_PATH = "Animations/";
+    private const string ANIMCTRL_PATH = "AnimatorControllers/";
+    
+    public AnimationManager(Animator animator)
+    {
+        avatarAnimator = animator;
+    }
+
+    public void LoadAnimatorControllerToAnimator(string animCtrlFilename)
+    {
+        avatarAnimator.runtimeAnimatorController = LoadAnimatorController(animCtrlFilename);
+    }
+
+    public void AssignAnimatorControllerToAnimator(UnityEditor.Animations.AnimatorController newAnimatorCtrl)
+    {
+        avatarAnimator.runtimeAnimatorController = newAnimatorCtrl;
+    }
+
+    // TODO: automatically play animation from first sentence to last
+    public void StartAnimation()
+    {
+        avatarAnimator.SetInteger(animCtrlParamName, 1);
+    }
+    
+    public UnityEditor.Animations.AnimatorController CreateAnimatorController(string name)
+    {
+        var controller = UnityEditor.Animations.AnimatorController.CreateAnimatorControllerAtPath("Assets/Resources/" + ANIMCTRL_PATH + name + ".controller");
+        controller.AddParameter(animCtrlParamName, AnimatorControllerParameterType.Int);
+        return controller;
+    }
+
+    public RuntimeAnimatorController LoadAnimatorController(string animCtrlFilename)
+    {
+        return Resources.Load(ANIMCTRL_PATH + animCtrlFilename) as RuntimeAnimatorController;
+    }
+
+    public void DeleteAnimatorController(RuntimeAnimatorController controller)
+    {
+        string path = AssetDatabase.GetAssetPath(controller);
+        Debug.Log("Delete: " + path);
+        AssetDatabase.DeleteAsset(path);
+    }
+
+    public UnityEditor.Animations.AnimatorState AddAnimationClip(UnityEditor.Animations.AnimatorController controller, string animationName)
+    {
+        // create a new state with the motion (animation clip) at root state machine
+        AnimationClip clip = Resources.Load(ANIMATION_PATH + animationName) as AnimationClip;
+        if (clip == null)
+        {
+            Debug.Log("Cannot find an animation clip called " + animationName);
+        }
+        else
+        {
+            Debug.Log(clip.ToString());
+        }
+
+        if (Resources.Load("Temp/" + animationName + ".fbx"))
+        {
+            Debug.Log("Found fbx: " + animationName);
+        }
+        else
+        {
+            Debug.Log("Cannot find fbx");
+        }
+
+        return controller.AddMotion(clip);
+    }
+
+    // Sentence level
+    public void CreateAnimatorStateTransitions(int sentenceNumber, UnityEditor.Animations.AnimatorState[] states)
+    {
+        UnityEditor.Animations.AnimatorState idleState = states[0];
+
+        for (int i = 0; i < states.Length; i++)
+        {
+            UnityEditor.Animations.AnimatorStateTransition transition;
+            if (i + 1 == states.Length)
+            {
+                // last state -> idle
+                transition = states[i].AddTransition(idleState);
+                transition.hasExitTime = true;
+            }
+            else if (i + 1 < states.Length)
+            {
+                transition = states[i].AddTransition(states[i + 1]);
+                transition.hasExitTime = true;
+
+                // add condition for changing the sentence
+                if (i == 0)
+                {
+                    transition.AddCondition(UnityEditor.Animations.AnimatorConditionMode.Equals, sentenceNumber, animCtrlParamName);
+                }
+            }
+        }
+
+    }
+}
