0033f4a6ea4f5d46b6052a9dbd1a9b356927926e
Nutthanich Narphromar <nutthanich.na@gmail.com>
2022-01-30 23:33:27 +0700

    Complete animation flow for playing a paragraph

--- ThSLtransProject/Assets/Scripts/AnimationManager.cs
+++ ThSLtransProject/Assets/Scripts/AnimationManager.cs
@@ -20,18 +20,29 @@ public class AnimationClipOverrides : List<KeyValuePair<AnimationClip, Animation
 
 public class AnimationManager
 {
-
     private Animator _avatarAnimator;
     private RuntimeAnimatorController _defaultAvatarAnimatorCtrl;
+    private bool _isSentencePosition1Started = false;
+    private bool _isSentencePosition2Started = false;
+    private int _playCount = 0;
     private string[] AnimStates1 = { "state1-1", "state2-1", "state3-1", "state4-1", "state5-1", "state6-1", "state7-1", "state8-1", "state9-1", "state10-1" };
     private string[] AnimStates2 = { "state1-2", "state2-2", "state3-2", "state4-2", "state5-2", "state6-2", "state7-2", "state8-2", "state9-2", "state10-2" };
 
     private const string AnimCtrlParamPlay = "Play";
     private const string AnimCtrlParamEnd = "End";
+    private const string AnimCtrlParamEnd2 = "End2";
+    private const string AnimCtrlParamIsP1Started = "IsP1Started";
+    private const string AnimCtrlParamIsP2Started = "IsP2Started";
     private const string AnimationPath = "Animations/";
     private const string AnimCtrlPath = "AnimatorControllers/";
 
+    private bool _isP1StartedPreviousState = false;
+    private bool _isP1StartedCurrentState = false;
+    private bool _isP2StartedPreviousState = false;
+    private bool _isP2StartedCurrentState = false;
+
     public Animator AvatarAnimator => _avatarAnimator;
+    public int SentencePlayCount => _playCount;
 
     public AnimationManager(Animator animator, string animCtrlName = null)
     {
@@ -43,9 +54,9 @@ public class AnimationManager
         _defaultAvatarAnimatorCtrl = _avatarAnimator.runtimeAnimatorController;
     }
 
-    public void PlayAnimation()
+    public void PlayAnimation(int sentencePosition)
     {
-        _avatarAnimator.SetInteger(AnimCtrlParamPlay, 1);
+        _avatarAnimator.SetInteger(AnimCtrlParamPlay, sentencePosition);
     }
 
     public void StopAnimation()
@@ -64,18 +75,94 @@ public class AnimationManager
         //ChangeAnimationState(ANIM_STATES[0]);
     }
 
+    // maybe use param to track if the first state of either sentence is played
+    public int GetCurrentPlayingSentencePosition()
+    {
+        if (_avatarAnimator.GetBool(AnimCtrlParamIsP1Started))
+        {
+            return 1;
+        }
+        else if (_avatarAnimator.GetBool(AnimCtrlParamIsP2Started))
+        {
+            return 2;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+
+    public bool IsAnimationStatePlayed(string stateName)
+    {
+        var layerNumber = 0;
+        return _avatarAnimator.GetCurrentAnimatorStateInfo(layerNumber).IsName(stateName);
+    }
+
+    public void UpdateSentencePositionStatus()
+    {
+        if (_avatarAnimator.GetCurrentAnimatorStateInfo(0).IsName("state1-1"))
+        {
+            _isSentencePosition1Started = true;
+        }
+        else if (_avatarAnimator.GetCurrentAnimatorStateInfo(0).IsName("state1-2"))
+        {
+            _isSentencePosition2Started = true;
+        }
+        else if (_avatarAnimator.GetCurrentAnimatorStateInfo(0).IsName("idle"))
+        {
+            _isSentencePosition1Started = false;
+            _isSentencePosition2Started = false;
+        }
+
+        _avatarAnimator.SetBool(AnimCtrlParamIsP1Started, _isSentencePosition1Started);
+        _avatarAnimator.SetBool(AnimCtrlParamIsP2Started, _isSentencePosition2Started);
+        _isP1StartedCurrentState = _isSentencePosition1Started;
+        _isP2StartedCurrentState = _isSentencePosition2Started;
+    }
+
+    public void UpdateSentencePlayCount()
+    {
+        if (_isP1StartedPreviousState != _isP1StartedCurrentState)
+        {
+            if (!_isP1StartedPreviousState)
+            {
+                _playCount++;
+                Debug.Log("[0] Play count: " + SentencePlayCount);
+            }
+            _isP1StartedPreviousState = _isP1StartedCurrentState;
+            
+        }
+        else if (_isP2StartedPreviousState != _isP2StartedCurrentState)
+        {
+            if (!_isP2StartedPreviousState)
+            {
+                _playCount++;
+                Debug.Log("[1] Play count: " + SentencePlayCount);
+            }
+            _isP2StartedPreviousState = _isP2StartedCurrentState;
+        }
+    }
+
     public void ResetAnimatorControllerStates()
     {
         Object.Destroy(_avatarAnimator.runtimeAnimatorController);
         _avatarAnimator.runtimeAnimatorController = _defaultAvatarAnimatorCtrl;
-        SetAnimationEndingPosition(AnimStates1.Length);
+        SetAnimationEndingPosition(AnimStates1.Length, 1);
+        SetAnimationEndingPosition(AnimStates2.Length, 2);
         ForceStopAnimation();
     }
 
     // Position starts at 1, unlike the index which starts at 0
-    public void SetAnimationEndingPosition(int targetStatePosition)
+    public void SetAnimationEndingPosition(int targetStatePosition, int sentencePosition)
     {
-        _avatarAnimator.SetInteger(AnimCtrlParamEnd, targetStatePosition);
+        if (sentencePosition == 1)
+        {
+            _avatarAnimator.SetInteger(AnimCtrlParamEnd, targetStatePosition);
+        }
+        else
+        {
+            _avatarAnimator.SetInteger(AnimCtrlParamEnd2, targetStatePosition);
+        }
     }
 
     public void LoadAnimatorControllerToAnimator(string animCtrlFilename)
@@ -94,7 +181,8 @@ public class AnimationManager
         if (animator == null)
         {
             return new AnimatorOverrideController(_avatarAnimator.runtimeAnimatorController);
-        } else
+        }
+        else
         {
             return new AnimatorOverrideController(animator.runtimeAnimatorController);
         }
@@ -130,12 +218,12 @@ public class AnimationManager
         return clip;
     }
 
-   public void OverrideAnimationClips(AnimatorOverrideController controller, string[] animClipNames, int sentencePosition)
+   public void OverrideAnimationClips(AnimatorOverrideController controller, List<string> animClipNames, int sentencePosition)
     {
         var currentOverrides = new AnimationClipOverrides(controller.overridesCount);
         controller.GetOverrides(currentOverrides);
 
-        for (int i = 0; i < animClipNames.Length; i++)
+        for (int i = 0; i < animClipNames.Count; i++)
         {
             AnimationClip newClip = LoadAnimationClip(animClipNames[i]);
             string defaultClipName = AnimStates1[i];
@@ -146,7 +234,6 @@ public class AnimationManager
             //Debug.Log($"{i} Default: {defaultClipName}");
             currentOverrides[defaultClipName] = newClip;
         }
-
         controller.ApplyOverrides(currentOverrides);
     }
 
@@ -156,7 +243,8 @@ public class AnimationManager
         if (sentencePosition == 1)
         {
             controller[AnimStates1[targetStateIndex]] = newClip;
-        } else
+        }
+        else
         {
             controller[AnimStates2[targetStateIndex]] = newClip;
         }
--- ThSLtransProject/Assets/Scripts/RainController.cs
+++ ThSLtransProject/Assets/Scripts/RainController.cs
@@ -4,50 +4,69 @@ using UnityEngine;
 
 public class RainController : MonoBehaviour
 {
-
     public bool ChangeSentence = false;
     public bool ChangeCtrl = false;
     public string DefaultAnimCtrlPath = "02RainAnimCtrl";
 
     private Animator _animator;
     private AnimationManager _animManager;
-    
+    private bool _switchSentence = false;
+    private bool _waitToUpdateAnimation = false;
+    private int _nextSentencePosition = 0;
+    private bool _reachEndOfParagraph = false;
+
     const string IdleAnim = "Rain@idle";
     const string MyselfAnim = "I";
     const string WalkAnim = "PERSONbody-WALK";
     const string YouAnim = "YOU";
     const string MoneyAnim = "MONEY";
-        
+    const string IdleState = "idle";
+
+    private AnimatorOverrideController _overrideController;
+    private int _sentenceIndex = 0;
+    private List<List<string>> _paragraph = new List<List<string>>()
+    {
+        new List<string>() { YouAnim, WalkAnim },
+        new List<string>() { MyselfAnim, MoneyAnim, WalkAnim, YouAnim },
+        new List<string>() { MoneyAnim, WalkAnim },
+        new List<string>() { YouAnim, MyselfAnim, WalkAnim, WalkAnim },
+        new List<string>() { MyselfAnim, MoneyAnim, YouAnim, MoneyAnim },
+    };
+
     // Start is called before the first frame update
     void Start()
     {
         _animator = GetComponent<Animator>();
         _animManager = new AnimationManager(_animator, DefaultAnimCtrlPath);
+        _overrideController = _animManager.CreateAnimatorOverrideController();
     }
 
     // Update is called once per frame
     void Update()
     {
+        _animManager.UpdateSentencePositionStatus();
+        _animManager.UpdateSentencePlayCount();
         
         if (Input.GetKeyDown(KeyCode.P))
         {
-            _animManager.PlayAnimation();
-            //animManager.ChangeAnimationState("walk01");
+            if (!_switchSentence)
+            {
+                _animManager.PlayAnimation(1);
+            } else
+            {
+                _animManager.PlayAnimation(2);
+            }
+            _switchSentence = !_switchSentence;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log(_animManager.GetCurrentPlayingSentencePosition());
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
             _animManager.ForceStopAnimation();
-        }
-
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            ChangeSentence = !ChangeSentence;
-            /*
-            if (defaultAnimCtrlPath == "RainAnimCtrl")
-            {
-                animator.SetBool("Change", changeSentence);
-            }*/
         }
 
         if (Input.GetKeyDown(KeyCode.C))
@@ -60,20 +79,102 @@ public class RainController : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.A))
         {
             Debug.Log("Assign new override anim ctrl");
+            //var overrideController = _animManager.CreateAnimatorOverrideController();
+            //var animNames = new List<string>() { YouAnim, WalkAnim };
+            //_animManager.OverrideAnimationClips(overrideController, animNames, 1);
+            //_animManager.OverrideSingleAnimationClip(overrideController, MoneyAnim, 3, 2);
 
-            var overrideController = _animManager.CreateAnimatorOverrideController();
-            string[] animNames = { YouAnim, WalkAnim };
-            _animManager.OverrideAnimationClips(overrideController, animNames, 1);
-            _animManager.OverrideSingleAnimationClip(overrideController, MoneyAnim, 3, 2);
+            //_animManager.PrintOverrideAnimationClips(overrideController);
+            //_animManager.AssignOverrideControllerToAvatarAnimatorController(overrideController);
 
-            _animManager.PrintOverrideAnimationClips(overrideController);
-            _animManager.AssignOverrideControllerToAvatarAnimatorController(overrideController);
-            _animManager.SetAnimationEndingPosition(4);
         }
 
         if (Input.GetKeyDown(KeyCode.X))
         {
             _animManager.ResetAnimatorControllerStates();
+        }
+
+        /* -------------------------------------- */
+
+        // update (or assign?) when reaching idle agian after 2 sentences are played
+        // use some param to check
+        if (_waitToUpdateAnimation && _animManager.IsAnimationStatePlayed(IdleState))
+        {
+            _animator.Update(0.0f);
+            _animManager.PlayAnimation(_nextSentencePosition);
+            _waitToUpdateAnimation = false;
+            Debug.Log($"Update animator. Set next position {_nextSentencePosition}");
+        }
+        
+        if (_sentenceIndex == 0)
+        {
+            // take care 1st sentence and 2nd sentence
+            for (int i = 0; i < _paragraph.Count; i++)
+            {
+                Debug.Log($"i: {i}, {i + 1}");
+                if (i > 1)
+                {
+                    break;
+                }
+                List<string> sentence = _paragraph[i];
+                _animManager.OverrideAnimationClips(_overrideController, sentence, i + 1);
+                _animManager.SetAnimationEndingPosition(sentence.Count, i + 1);
+                _sentenceIndex++;
+            }
+            _animManager.AssignOverrideControllerToAvatarAnimatorController(_overrideController);
+            _animManager.PlayAnimation(1);
+            _animManager.ChangeAnimationState("state1-1");
+            _waitToUpdateAnimation = true;
+            _nextSentencePosition = 2;
+            Debug.Log($"Initialize override controller. Current sentenceIndex: {_sentenceIndex}, p count: {_paragraph.Count}");
+        }
+        // take care 3rd sentence until the last sentence
+        else if (_sentenceIndex < _paragraph.Count)
+        {
+            //Debug.Log("index < count");
+            List<string> sentence = _paragraph[_sentenceIndex];
+            int sentenceLength = sentence.Count;
+            //int targetSentencePosition;
+
+            if (_sentenceIndex % 2 == 0)
+            {
+                // even index = odd position
+                if (_animManager.GetCurrentPlayingSentencePosition() == 2 && !_waitToUpdateAnimation)
+                {
+                    // safe to override anim
+                    Debug.Log("Current state: " + _animManager.GetCurrentPlayingSentencePosition());
+                    _animManager.OverrideAnimationClips(_overrideController, sentence, 1);
+                    _animManager.SetAnimationEndingPosition(sentenceLength, 1);
+                    _waitToUpdateAnimation = true;
+                    _nextSentencePosition = 1;
+                    Debug.Log($"Override position 1 with sentenceIndex {_sentenceIndex}");
+                    _sentenceIndex++;
+                }
+            }
+            else
+            {
+                // odd index = even position
+                if (_animManager.GetCurrentPlayingSentencePosition() == 1 && !_waitToUpdateAnimation)
+                {
+                    // safe to override anim
+                    _animManager.OverrideAnimationClips(_overrideController, sentence, 2);
+                    _animManager.SetAnimationEndingPosition(sentenceLength, 2);
+                    _waitToUpdateAnimation = true;
+                    _nextSentencePosition = 2;
+                    Debug.Log($"Override position 2 with sentenceIndex {_sentenceIndex}");
+                    _sentenceIndex++;
+                }
+            }
+        }
+        else
+        {
+            if (_animManager.SentencePlayCount == _paragraph.Count && !_reachEndOfParagraph)
+            {
+                Debug.Log("All sentences are played");
+                _reachEndOfParagraph = true;
+                _waitToUpdateAnimation = true;
+                _nextSentencePosition = 0;
+            }
         }
     }
 }
